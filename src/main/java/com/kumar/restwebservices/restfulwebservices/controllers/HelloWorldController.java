4475e1a656822cb44ea447de3642e4090e27a2de
DK2698 <dileepkumar1823@gmail.com>
2023-01-24 16:29:25 +0530

    to use i18n

--- src/main/java/com/kumar/restwebservices/restfulwebservices/controllers/HelloWorldController.java
+++ src/main/java/com/kumar/restwebservices/restfulwebservices/controllers/HelloWorldController.java
@@ -1,10 +1,20 @@
 package com.kumar.restwebservices.restfulwebservices.controllers;
 
 import com.kumar.restwebservices.restfulwebservices.beans.HelloWorldBean;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+
+    private MessageSource messageSource;
+
+    public  HelloWorldController(MessageSource messageSource){
+        this.messageSource = messageSource;
+    }
 
     @RequestMapping(method = RequestMethod.GET,path = "/hello-world")
     public  String helloWorld(){
@@ -28,4 +38,11 @@ public class HelloWorldController {
         return new HelloWorldBean(
                 String.format( "hello world, %s ", name));
     }
+
+    @GetMapping(path = "/hello-worldi18n")
+    public  String helloWorldI18n(){
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default message", locale);
+    }
+    // 1: good.morning.message
 }
